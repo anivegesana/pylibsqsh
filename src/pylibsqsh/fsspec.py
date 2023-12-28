fc4f1144218b76180075488a7c4d4b2e80d24b89
anivegesana <anirudh.vegesana@gmail.com>
2023-12-27 23:57:11 -0800

    Read from squashfs

---
+++ src/pylibsqsh/fsspec.py
@@ -0,0 +1,64 @@
+import fsspec
+from fsspec.archive import AbstractArchiveFileSystem
+from io import TextIOWrapper
+
+from .pylibsqsh_ext import SqshArchive, SqshFile
+
+
+class SquashFSFileSystem(AbstractArchiveFileSystem):
+    root_marker = ""
+    protocol = "squashfs"
+    cachable = True
+
+    def __init__(
+        self,
+        fo="",
+        mode="r",
+        target_protocol=None,
+        target_options=None,
+        **kwargs,
+    ):
+        super().__init__()
+        if mode != 'r':
+            raise ValueError(f"mode '{mode}' no understood")
+        if isinstance(fo, str):
+            fo = fsspec.open(
+                fo, mode=mode, protocol=target_protocol, **(target_options or {})
+            )
+        self.of = fo
+        self.fo = fo.__enter__()  # the whole instance is a context
+        if not isinstance(self.fo, TextIOWrapper):
+            raise ValueError("Currently, only files on disks can be read.")
+        self.sqshfs = SqshArchive(self.fo.name)
+
+    def __del__(self):
+        if hasattr(self, "sqshfs"):
+            self.close()
+            del self.sqshfs
+
+    def close(self):
+        """Commits any write changes to the file. Done on ``del`` too."""
+        self.sqshfs.close()
+
+    @classmethod
+    def _strip_protocol(cls, path):
+        # zip file paths are always relative to the archive root
+        return super()._strip_protocol(path).lstrip("/")
+
+    # _get_dirs
+
+    def _open(
+        self,
+        path: str,
+        mode="rb",
+        block_size=None,
+        autocommit=True,
+        cache_options=None,
+        **kwargs,
+    ) -> SqshFile:
+        assert mode in ("rb", "r")
+        assert block_size is None
+        assert autocommit
+        return self.sqshfs.open(path)
+
+fsspec.register_implementation('squashfs', SquashFSFileSystem)
--- src/pylibsqsh_ext.cpp
+++ src/pylibsqsh_ext.cpp
@@ -2,6 +2,8 @@
 #include <nanobind/stl/string.h>
 #include "sqsh.h"
 
+#include "sqsh_easy.h"
+
 namespace nb = nanobind;
 
 using namespace nb::literals;
@@ -9,12 +11,73 @@ using namespace nb::literals;
 #define PYSQSH_CHECK_CLOSED(fd) if (fd == NULL) { nb::raise("File is already closed!"); }
 #define PYSQSH_RAISE(err) { nb::raise("%s", sqsh_error_str(err)); }
 
+
+nb::bytes _pysqsh_file_content(struct SqshFile *file) {
+    // sqsh_easy_file_content but uses SqshFile instead of SqshArchive
+	int rv = 0;
+	uint8_t *content = NULL;
+    struct SqshFileIterator* iterator;
+    size_t file_size;
+
+	if (rv < 0) {
+		goto out;
+	}
+
+	iterator = sqsh_file_iterator_new(file, &rv);
+	if (rv < 0) {
+		goto out;
+	}
+
+	file_size = sqsh_file_size(file);
+	content = (uint8_t*)calloc(file_size + 1, sizeof(*content));
+	if (content == NULL) {
+		rv = -SQSH_ERROR_MALLOC_FAILED;
+		goto out;
+	}
+
+	for (sqsh_index_t pos = 0;
+		 sqsh_file_iterator_next(iterator, SIZE_MAX, &rv);) {
+		const uint8_t *data = sqsh_file_iterator_data(iterator);
+		const size_t size = sqsh_file_iterator_size(iterator);
+		memcpy(&content[pos], data, size);
+		pos += size;
+	}
+
+out:
+	sqsh_file_iterator_free(iterator);
+	if (rv < 0) {
+		free(content);
+        PYSQSH_RAISE(rv);
+	}
+    nb::bytes out = nb::bytes((const char *)content, file_size);
+	return out;
+}
+
+// class PySqshFile;
+
+// class PySqshFileIterator {
+//     struct SqshFileIterator* itr;
+// public:
+//     PySqshFileIterator(PySqshFile file) {
+//         int err;
+//         itr = sqsh_file_iterator_new(file.subfd, &err);
+//         if (itr == NULL) PYSQSH_RAISE(err);
+//     }
+//     inline void close() {
+//         PYSQSH_CHECK_CLOSED(itr);
+//         int err = sqsh_file_iterator_free(itr);
+//         if (err != 0) PYSQSH_RAISE(err);
+//         itr = NULL;
+//     }
+// }
+
 class PySqshFile {
     struct SqshFile* subfd;
     friend class PySqshArchive;
     PySqshFile(struct SqshFile* subfd) : subfd(subfd) {}
 public:
-    inline void close() {
+    inline void close(bool idempotent = false) {
+        if (!idempotent) PYSQSH_CHECK_CLOSED(subfd);
         int err = sqsh_close(subfd);
         if (err != 0) PYSQSH_RAISE(err);
         subfd = NULL;
@@ -72,19 +135,31 @@ public:
         PYSQSH_CHECK_CLOSED(subfd);
         return sqsh_file_size(subfd);
     }
+    inline nb::bytes read() {
+        PYSQSH_CHECK_CLOSED(subfd);
+        return _pysqsh_file_content(subfd);
+    }
 };
 
 class PySqshArchive {
     struct SqshArchive* fd;
 public:
     PySqshArchive(const std::string path) {
-        PYSQSH_CHECK_CLOSED(fd);
         int err;
         fd = sqsh_archive_open(path.data(), NULL, &err);
         if (fd == NULL) PYSQSH_RAISE(err);
     }
-    inline void close() {
-        PYSQSH_CHECK_CLOSED(fd);
+    PySqshArchive(const nb::bytes buffer) {
+        int err;
+        struct SqshConfig config = {
+            .source_mapper = sqsh_mapper_impl_static,
+            .source_size = buffer.size()
+        };
+        fd = sqsh_archive_open(buffer.ptr(), NULL, &err);
+        if (fd == NULL) PYSQSH_RAISE(err);
+    }
+    inline void close(bool idempotent = false) {
+        if (!idempotent) PYSQSH_CHECK_CLOSED(fd);
         int err = sqsh_archive_close(fd);
         if (err != 0) PYSQSH_RAISE(err);
     }
@@ -101,6 +176,24 @@ public:
         struct SqshFile* subfd = sqsh_open_by_ref(fd, inode, &err);
         if (subfd == NULL) PYSQSH_RAISE(err);
         return PySqshFile(subfd);
+    }
+    inline size_t easy_file_size(const std::string path) {
+        PYSQSH_CHECK_CLOSED(fd);
+        int err;
+        size_t size = sqsh_easy_file_size(fd, path.data(), &err);
+        if (size == 0) PYSQSH_RAISE(err);
+        return size;
+    }
+    inline nb::bytes easy_content(const std::string path) {
+        PYSQSH_CHECK_CLOSED(fd);
+        int err;
+        size_t size = sqsh_easy_file_size(fd, path.data(), &err);
+        if (size == 0) PYSQSH_RAISE(err);
+        uint8_t* content = sqsh_easy_file_content(fd, path.data(), &err);
+        if (content == NULL) PYSQSH_RAISE(err);
+        nb::bytes out = nb::bytes((const char *)content, size);
+        free(content);
+        return out;
     }
 };
 
@@ -119,7 +212,9 @@ NB_MODULE(pylibsqsh_ext, m) {
         // .def("open", &PySqshArchive::open)
         .def("open", [&](PySqshArchive& r, const std::string path) { return r.open(path); } )
         .def("open", [&](PySqshArchive& r, uint64_t inode) { return r.open(inode); } )
-        .def("close", [&](PySqshArchive& r) { r.close(); } );
+        .def("close", [&](PySqshArchive& r) { r.close(); } )
+        .def("__del__", [&](PySqshArchive& r) { r.close(true); } )
+        .def("_easy_content", [&](PySqshArchive& r, const std::string path) { return r.easy_content(path); } );
 
     nb::class_<PySqshFile>(m, "SqshFile")
         .def("__enter__", [&](PySqshFile& r) { return r; } )
@@ -132,6 +227,8 @@ NB_MODULE(pylibsqsh_ext, m) {
             "traceback"_a.none()
         )
         .def("close", [&](PySqshFile& r) { r.close(); } )
+        .def("__del__", [&](PySqshFile& r) { r.close(true); } )
+        .def("read", [&](PySqshFile& r) { return r.read(); } )
         .def_prop_ro("uid", [&](PySqshFile& r) { return r.get_uid(); } )
         .def_prop_ro("gid", [&](PySqshFile& r) { return r.get_gid(); } )
         .def_prop_ro("type", [&](PySqshFile& r) { return r.get_type(); } )
